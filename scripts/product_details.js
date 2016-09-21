f806de5e2fbe1a86425161b3656bd0ac9a7246e6
Alexandre NICOL <nemokervi@yahoo.fr>
2016-09-21 10:14:46 +1000

    good product pages

--- scripts/product_details.js
+++ scripts/product_details.js
@@ -1,6 +1,8 @@
 var slide_distance_percentage;
 var number_of_picture;
 var actual_position = 0;
+var sizes = [];
+
 // Sliding des images
 function slide_image( next_position ){
 	$('section#product_image ul.dot li').removeClass('active');
@@ -8,12 +10,28 @@ function slide_image( next_position ){
 		case 'right':
 			if(actual_position <= number_of_picture-2){
 				$('section#product_image ul.gallery').css('transform', 'translateX(' + -((actual_position+1)*slide_distance_percentage) + '%)');
+				
+				$('section#product_information ul.tag').html('');
+				gallery_content[(actual_position+1)].tag.forEach(function(item, index){
+					$('section#product_information ul.tag').prepend('<li><img src="../../../images/BADGE_' + item + '.svg" alt=""></li>');
+				});
+				$('section#product_information p.color').text(gallery_content[actual_position].color);
+				$('section#product_information p.size').text(sizes[actual_position]);
+
 				actual_position = (actual_position + 1);
 			}
 			break;
 		case 'left':
 			if(actual_position >= 1){
 				$('section#product_image ul.gallery').css('transform', 'translateX(' + -((actual_position-1)*slide_distance_percentage) + '%)');
+
+				$('section#product_information ul.tag').html('');
+				gallery_content[(actual_position-1)].tag.forEach(function(item, index){
+					$('section#product_information ul.tag').prepend('<li><img src="../../../images/BADGE_' + item + '.svg" alt=""></li>');
+				});
+				$('section#product_information p.color').text(gallery_content[actual_position].color);
+				$('section#product_information p.size').text(sizes[actual_position]);
+
 				actual_position = (actual_position - 1);
 			}
 			break;
@@ -23,22 +41,31 @@ function slide_image( next_position ){
 			break;
 	}
 	$('section#product_image ul.dot li:nth-child(' + (actual_position+1) + ')').addClass('active');
-	console.log( actual_position + ' | ' + next_position + ' | ' + slide_distance_percentage);
+	//console.log( actual_position + ' | ' + next_position + ' | ' + slide_distance_percentage);
 }
 
 // Genration of the gallery
 function init_gallery(){
-	number_of_picture = gallery_picture.length;
-	slide_distance_percentage = (100 / gallery_picture.length);
+	number_of_picture = gallery_content.length;
+	slide_distance_percentage = (100 / gallery_content.length);
 	$('section#product_image ul.gallery').css('width', number_of_picture + '00%');
 	$('section#product_image ul.dot').css('width', number_of_picture*17 + 'px');
 
 	for(var i=0;  i < number_of_picture; i++) {
-		// console.log(gallery_picture[i]);
-		console.log(((number_of_picture-1)-i) + ' | ' + i + ' | ' + number_of_picture);
-		$('section#product_image ul.gallery').prepend('<li style="width: ' + slide_distance_percentage + '%;"><img src="../../images/products/' + gallery_picture[i] + '.png" alt="">');
+		$('section#product_image ul.gallery').prepend('<li style="width: ' + slide_distance_percentage + '%;"><img src="../../../images/products/' + gallery_content[i].picture + '.png" alt="">');
 		if(i == number_of_picture-1){$('section#product_image ul.dot').prepend('<li class="active"><a onclick="slide_image(' + ((number_of_picture-1)-i) + ')" href="#"></a></li>');}else{$('section#product_image ul.dot').prepend('<li><a onclick="slide_image(' + i + ')" href="#"></a></li>');}
+
+		var size_length = gallery_content[i].size.length;
+		var size =  gallery_content[i].size[0] + ' - ' + gallery_content[i].size[(size_length-1)];
+		sizes.push(size)
 	}
+	gallery_content[0].tag.forEach(function(item, index){
+		$('section#product_information ul.tag').prepend('<li><img src="../../../images/BADGE_' + item + '.svg" alt=""></li>');
+	});
+
+	$('section#product_information p.color').text(gallery_content[0].color);
+	$('section#product_information p.size').text(sizes[0]);
+
 
 }
 init_gallery();
